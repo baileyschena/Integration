cbadd0c3b4c04025ecf6611b2a61475f27dc952b
baileyschena <baileyschena@gmail.com>
2019-09-17 12:23:23 -0400

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,1 +1,29 @@
-print ("1")
+#Inegration by Bailey Schena
+#Program is going to start as a quiz or riddle thing.
+print("Hello! Welcome to my easy quiz")
+print("What is the order of operations?")
+print("1. PEMDAS")
+print("2. PEMSAD")
+print("3. DASMEP")
+print("4. Whats an operation?")
+answer = input("To select an answer, type in the corresponding number, and press enter: ")
+
+answernum = int(answer)
+print(answernum)
+if(answernum == 1):
+    print("Good job! The answer is correct!")
+else:
+    print("Sorry, the answer is incorrect.")
+
+
+print("What is 5-2?")
+answer = input("Type the answer, and hit enter: ")
+
+answernum = int(answer)
+print(answernum)
+if(answernum == 3):
+    print("Good job! The answer is correct!")
+else:
+    print("Sorry, the answer is incorrect.")
+
+

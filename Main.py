18bf7d82fe6a911db3890daae217612e66f82027
baileyschena <baileyschena@gmail.com>
2019-09-17 12:53:47 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,13 +1,13 @@
 #Inegration by Bailey Schena
 #Program is going to start as a quiz or riddle thing.
 print("Hello! Welcome to my easy quiz")
+#Question One (Multiple Choice)
 print("What is the order of operations?")
 print("1. PEMDAS")
 print("2. PEMSAD")
 print("3. DASMEP")
 print("4. Whats an operation?")
 answer = input("To select an answer, type in the corresponding number, and press enter: ")
-
 answernum = int(answer)
 print(answernum)
 if(answernum == 1):
@@ -15,7 +15,7 @@ if(answernum == 1):
 else:
     print("Sorry, the answer is incorrect.")
 
-
+#Question Two (Math Problem)
 print("What is 5-2?")
 answer = input("Type the answer, and hit enter: ")
 
